3febbb0c2916da2c8ec38a3ae462ded6777fe673
Peter Wishart <peter.wishart@zonal.co.uk>
2019-03-21 16:58:03 +0000

    Commit first version

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # StreamConnect
-Test utility to create a layer 4 bridge from a pair of RS232, named pipe and TCP/IP streams
+Test utility to create a layer 4-type bridge from a pair of RS232, named pipe or TCP/IP streams, on Windows OS.
+
+RS232 functionality is a bit limited - no error detection or flow control.
+
+# Building
+
+* Install Visual Studio code
+* Install .Net SDK (https://dotnet.microsoft.com/learn/dotnet/hello-world-tutorial#install)
+* Install the .Net 4.5 runtime (e.g. if building on Windows 7 or older)
+* Open the project folder in VS code
+* In VS code, start a terminal and run "dotnet build" to build the app
+
+The build should output:
+
+```PS c:\git\IpToSerial\StreamConnect\ip_to_serial_emulator\StreamConnect> dotnet build
+Microsoft (R) Build Engine version 15.9.20+g88f5fadfbe for .NET Core
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+  Nothing to do. None of the projects specified contain packages to restore.
+  StreamConnect -> c:\git\IpToSerial\StreamConnect\bin\Debug\StreamConnect.exe
+
+Build succeeded.
+    0 Warning(s)
+    0 Error(s)
+
+Time Elapsed 00:00:02.02
+```
+
+# Running the app
+ * Run the app with a pair of stream endpoint specifications: `bin\debug\StreamConnect.exe <SourceStream> <DestinationStream>`
+ * RS232 comms uses 115200 baud, with DTR raised while a client is connected and is specified in the form "COMxx" where x is the COM port
+ * Named pipe comms is specified as `\\.\pipe\PipeNameHere`
+ * A socket endpoint is specified as `IP:port` where IP is a dotted IPv4 address
+
+For example, when started with `bin\debug\StreamConnect.exe 0.0.0.0:49153 COM1`, the app will accept incoming TCP/IP connections and when connected, redirect data to/from the local COM port.
+
+Output should be like this:
+```PS C:\git\StreamConnect> .\bin\debug\StreamConnect.exe 0.0.0.0:1234 COM1
+Source Socket 0.0.0.0:1234 -> Dest Serial COM1
+Connection from Socket 0.0.0.0:1234
+Connection made
+............................Input stream disconnected
+```
+
+NB the tool echoes dots to the console as data is forwarded.
+
+# Why?
+
+I use it for redirecting / monitoring serial port usage from VMs when talking to fixed-function RS232 devices, and for emulating IPToSerial devices.
+
+# Running with virtual serial ports
+
+The app can also be used to map VMWare virtual serial ports to local COM ports, see Program.cs for details.
---
+++ StreamCrossover.cs
@@ -0,0 +1,98 @@
+﻿namespace StreamConnect
+{
+    using System;
+    using System.Diagnostics;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class StreamCrossover
+    {
+        private const int MaxMessageSize = 1024;
+
+        [Conditional("OperationDebugs")]
+        private static void OpDiag(Operation op, int byteCount, byte[] buffer)
+        {
+            var tmpBuffer = new byte[byteCount];
+            Array.Copy(buffer, tmpBuffer, byteCount);
+            for (int i = 0; i < byteCount; i++)
+                if (tmpBuffer[i] < 32)
+                    tmpBuffer[i] = 32;            
+
+            var sb = new StringBuilder(byteCount);
+            sb.Append("0x");
+            if (byteCount > 16)
+            {
+                for (int i = 0; i < 8; i++)                
+                    sb.Append(buffer[i].ToString("x2"));
+                sb.Append("..");
+                for (int i = byteCount - 8; i < byteCount; i++)
+                    sb.Append(buffer[i].ToString("x2"));
+            }
+            else
+            {
+                for (int i = 0; i < byteCount; i++)               
+                    sb.Append(buffer[i].ToString("x2"));                
+            }
+            Console.WriteLine(String.Format("{0}\t{1}\t{2}", Enum.GetName(typeof(Operation), op), byteCount, sb));
+        }
+
+        private enum Operation
+        {
+            OutboundWrite,
+            OutboundRead,
+            InboundRead,
+            InboundWrite
+        }
+
+        // handle sync reads from each end and pass to the other end
+        public static async Task HandleClientAsync(CancellationToken cancelToken, IPluggableStreamAsync inputStream, IPluggableStreamAsync outputStream)
+        {
+            while (!cancelToken.IsCancellationRequested)
+            {             
+                byte[] outputRead = new byte[MaxMessageSize];
+                byte[] inputRead = new byte[MaxMessageSize];
+
+                var readFromOutput = outputStream.ReadAsync(outputRead, 0, MaxMessageSize, cancelToken);
+                var readFromInput = inputStream.ReadAsync(inputRead, 0, MaxMessageSize, cancelToken);
+                while (!cancelToken.IsCancellationRequested)
+                {
+                    int byteCount;
+                    var nextTask = await Task.WhenAny(new[] { readFromOutput, readFromInput });
+                    Console.Write(".");
+                    if (nextTask == readFromInput)
+                    {
+                        try
+                        {
+                            byteCount = await nextTask;
+                        }
+                        catch (System.IO.IOException e) when ((e.InnerException as SocketException)?.ErrorCode == 10054)
+                        {
+                            throw new OperationCanceledException("Input stream disconnected (WSACONNRESET)");
+                        }
+                        OpDiag(Operation.InboundRead, byteCount, inputRead);
+                        if (byteCount == 0)
+                        {
+                            throw new OperationCanceledException("Input stream disconnected");
+                        }
+                        var outputWrite = new byte[MaxMessageSize];
+                        Array.Copy(inputRead, outputWrite, byteCount);
+                        var backTask = outputStream.WriteAsync(outputWrite, 0, byteCount, cancelToken);
+                        readFromInput = inputStream.ReadAsync(inputRead, 0, MaxMessageSize, cancelToken);
+                    }
+                    else
+                    if (nextTask == readFromOutput)
+                    {
+                        byteCount = await nextTask;
+                        OpDiag(Operation.OutboundRead, byteCount, outputRead);
+                        var inputWrite = new byte[MaxMessageSize];
+                        Array.Copy(outputRead, inputWrite, byteCount);
+                        var backTask = inputStream.WriteAsync(inputWrite, 0, byteCount, cancelToken);
+                        readFromOutput = outputStream.ReadAsync(outputRead, 0, MaxMessageSize, cancelToken);
+                    }
+                }       
+            }
+        }
+    }
+}
